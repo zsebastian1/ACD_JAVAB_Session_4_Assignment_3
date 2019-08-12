db220a53a2b78d82e376b1fb6b4028744363ee72
zsebastian1 <44416197+zsebastian1@users.noreply.github.com>
2019-08-12 10:13:36 -0400

    Add files via upload

---
+++ Assignment4_3.java
@@ -0,0 +1,31 @@
+package ACADAssignments;
+
+public class Assignment4_3 {
+
+	public static void main(String[] args) {
+
+
+		Assignment4_3 a43 = new Assignment4_3();
+		System.out.println(a43.theSum(4, 6, 8));
+		
+		}
+		
+	
+	
+	public  int theSum(int x) {
+		int sum = 0;
+		sum = x;
+		return x;
+	}
+
+	public  int theSum(int x, int y) {
+	int	sum = x + y;
+		return sum;
+	}
+	public  int theSum(int x, int y, int z) {
+
+	return	this.theSum(3,4);
+		//int sum = x + y + z;
+		//return sum;
+	}
+}
